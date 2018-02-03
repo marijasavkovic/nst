c1819991145a2538579a75bf5fe8062d97cc21b6
Marija.Savkovic <marija.savkovic@plusplusnt.rs>
2018-02-03 23:58:34 +0100

    New project

---
+++ src/main/java/com/master/nst/NstApplication.java
@@ -0,0 +1,12 @@
+package com.master.nst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NstApplication.class, args);
+	}
+}
